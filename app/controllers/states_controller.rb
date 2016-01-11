5a319550f71dc1819a44c7deeba92bb72ba9cae0
kofran <kofran.amd@gmail.com>
2016-01-11 20:55:10 -0300

    first commit

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,22 @@
+class StatesController < ApplicationController
+  def index
+    @states = State.all.order(name: :asc)
+  end
+
+  def new
+    @state = State.new
+  end
+
+  def create
+    @state = State.new(state_params)
+    if @state.save
+      redirect_to :back
+    else
+      render :new
+    end
+  end
+
+  def state_params
+    params.require(:state).permit(:name)
+  end
+end
---
+++ db/migrate/20160111214105_add_state_id_to_cities.rb
@@ -0,0 +1,5 @@
+class AddStateIdToCities < ActiveRecord::Migration
+  def change
+    add_reference :cities, :state, index: true, foreign_key: true
+  end
+end
